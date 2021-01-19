3ca8e425f52767c6d59f4e3095ca24e0b12f2d0b
Daniel Batista <dani_batista20@hotmail.com>
2021-01-19 11:31:34 +0000

    add: todos & todo-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+
 import { TodosComponent } from './components/todos/todos/todos.component';
+import { TodoItemComponent } from './components/todos/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/todos/todos/todos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  title:string = 'Todos list';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
